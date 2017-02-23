2f8001e94ffb3a51fd35f5e0ff8696b324d72a7f
misteryeo <andy.yeo@gmail.com>
2017-02-23 12:29:41 -0800

    Sends a simple get request (no URL)

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax ({
+    url: '',
+    type: 'GET',
+    contentType: 'application/json',
+    success: (data) => {
+      console.log('successful data fetch!');
+      console.log(data); //set this to window.exampleVideoData equiv
+    },
+    error: (data) => {
+      console.error('bad dog, no biscuit', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
