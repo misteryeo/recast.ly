7ed62dd16f7356af5f587606a659ca126dab94b1
misteryeo <andy.yeo@gmail.com>
2017-02-22 21:09:12 -0800

    Pass stateful class, single VideoPlayer and VideoList tests.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,36 @@
-var App = (props) => {
-  console.log(props.videoData);
-  return (
-    <div>
-      <Nav />
-      <div className="col-md-7">
-        <VideoPlayer video={props.videoData[0]}/>
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentVideo: window.exampleVideoData[0],
+      videoList: window.exampleVideoData
+    };
+  }
+
+  onVideoClick (event) {
+    console.log('CLICKED!');
+    this.setState({
+      currentVideo: window.exampleVideoData[0], //set the clicked video to the current video
+      videoList: window.exampleVideoData
+    });
+  }
+
+  render() {
+    console.log(this);
+    console.log(this.state.currentVideo);
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList}/>
+        </div>
       </div>
-      <div className="col-md-5">
-        <VideoList videos={props.videoData}/>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
